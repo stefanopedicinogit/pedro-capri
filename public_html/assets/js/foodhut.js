4a75ecca17733f4fa9ce543aafa1d3f76dbd1c4a
Stefano Pedicino <83118026+stefanopedicinogit@users.noreply.github.com>
2025-03-09 00:06:30 +0100

    PedroCapri v0.99

--- public_html/assets/js/foodhut.js
+++ public_html/assets/js/foodhut.js
@@ -44,3 +44,59 @@ function initMap() {
       map: map
     });
  }
+
+
+ // Back to top button
+ $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn('slow');
+    } else {
+        $('.back-to-top').fadeOut('slow');
+    }
+});
+$('.back-to-top').click(function () {
+    $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+    return false;
+});
+
+
+
+function fetchImages() {
+    const folderPath = 'assets/img/';
+    const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
+    const sliderContainer = document.querySelector('.slider-container');
+    const slider = sliderContainer.querySelector('.slider');
+
+    fetch(folderPath, {
+        headers: {
+            Accept: 'application/json'
+        }
+    })
+        .then(response => response.text())
+        .then(data => {
+            const files = JSON.parse(data); // Parse the data string into a JSON array
+            const images = [];
+    
+            files.forEach((file, index) => {
+                const filePath = folderPath + file;
+                const fileExtension = file.split('.').pop().toLowerCase();
+                if (imageExtensions.includes(fileExtension)) {
+                    const img = document.createElement('img');
+                    img.src = filePath;
+                    img.id = `slide-${index + 1}`; // Add an id attribute
+                    images.push(img);
+                    slider.appendChild(img); // Add the img element to the slider container
+                }
+            });
+
+            // Create navigation links for each image
+            const sliderNav = sliderContainer.querySelector('.slider-nav');
+            images.forEach((img, index) => {
+                const navLink = document.createElement('a');
+                navLink.href = `#slide-${index + 1}`;
+                sliderNav.appendChild(navLink);
+            });
+        })
+}
+
+fetchImages();
